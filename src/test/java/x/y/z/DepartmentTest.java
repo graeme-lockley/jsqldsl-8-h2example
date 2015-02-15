3513e1e6985a82cfeeaca4ccc7a24fa4c6e34e19
Graeme Lockley <graemel@no9.co.za>
2015-02-15 07:44:00 +0200

    Generate for the entire H2 schema and added a simple test for department to ensure that the generators are working properly.

---
+++ src/test/java/x/y/z/DepartmentTest.java
@@ -0,0 +1,18 @@
+package x.y.z;
+
+import org.junit.Test;
+import za.co.no9.jsqldsl.db.h2.Query;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+public class DepartmentTest {
+    @Test
+    public void should_show_simple_query() throws Exception {
+        assertEquals(
+                "SELECT d.ACTIVE, d.ID, d.NAME FROM DEPARTMENT AS d WHERE d.ID > 10 AND d.NAME IS NOT NULL",
+                Query.from(DEPARTMENT.as("d"))
+                        .where(d -> d.ID.gt(10).and(d.NAME.isNotNull()))
+                        .select(d -> asList(d.ACTIVE, d.ID, d.NAME)).asString());
+    }
+}
