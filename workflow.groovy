1823d14b8b5f0ca93cca51c50de07e2fbc2ed28f
Graeme Lockley <graemel@no9.co.za>
2015-02-15 07:44:52 +0200

    Store the test results

--- workflow.groovy
+++ workflow.groovy
@@ -1,6 +1,8 @@
 def execute() {
     stage 'Build - clean install'
     sh 'mvn clean install'
+
+    step $class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'
 }
 
 return this;
